fb701bd68917a15e61dcd17c56ecd51197b39f05
Pierrick Roger <pk.roger@icloud.com>
2018-06-29 16:10:35 +0200

    Update test script with new tool parameters.

--- test-qualitymetrics.sh
+++ test-qualitymetrics.sh
@@ -4,13 +4,13 @@
 scriptdir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
 
 # Test with fixed old style naming for sampleType
-Rscript $scriptdir/qualitymetrics_wrapper.R dataMatrix_in "$scriptdir/test-data/input-dataMatrix.tsv" sampleMetadata_in "$scriptdir/test-data/input-sampleMetadata.tsv" variableMetadata_in "$scriptdir/test-data/input-variableMetadata.tsv" CV "FALSE" Compa "TRUE" seuil "1" poolAsPool1L "TRUE" sampleMetadata_out "$scriptdir/output-sampleMetadata.tsv" variableMetadata_out "$scriptdir/output-variableMetadata.tsv" sampleTypeColName sampleType sampleTypeTags blank=blank,pool=pool,sample=sample || exit 1
+Rscript $scriptdir/qualitymetrics_wrapper.R dataMatrix_in "$scriptdir/test-data/input-dataMatrix.tsv" sampleMetadata_in "$scriptdir/test-data/input-sampleMetadata.tsv" variableMetadata_in "$scriptdir/test-data/input-variableMetadata.tsv" CV "FALSE" Compa "TRUE" seuil "1" poolAsPool1L "TRUE" sampleMetadata_out "$scriptdir/output-sampleMetadata.tsv" variableMetadata_out "$scriptdir/output-variableMetadata.tsv" injectionOrderColName injectionOrder batchColName batch sampleTypeColName sampleType sampleTypeTags blank=blank,pool=pool,sample=sample || exit 1
 
 diff $scriptdir/output-sampleMetadata.tsv $scriptdir/test-data/output-sampleMetadata.tsv || exit 2
 diff $scriptdir/output-variableMetadata.tsv $scriptdir/test-data/output-variableMetadata.tsv || exit 2
 
 # Test with custom naming for sampleType
-Rscript $scriptdir/qualitymetrics_wrapper.R dataMatrix_in "$scriptdir/test-data/input-dataMatrix.tsv" sampleMetadata_in "$scriptdir/test-data/input-sampleMetadata-customSampleType.tsv" variableMetadata_in "$scriptdir/test-data/input-variableMetadata.tsv" CV "FALSE" Compa "TRUE" seuil "1" poolAsPool1L "TRUE" sampleMetadata_out "$scriptdir/output-sampleMetadata.tsv" variableMetadata_out "$scriptdir/output-variableMetadata.tsv" sampleTypeColName MySampType sampleTypeTags blank=blanc,pool=lot,sample=echant || exit 1
+Rscript $scriptdir/qualitymetrics_wrapper.R dataMatrix_in "$scriptdir/test-data/input-dataMatrix.tsv" sampleMetadata_in "$scriptdir/test-data/input-sampleMetadata-customSampleType.tsv" variableMetadata_in "$scriptdir/test-data/input-variableMetadata.tsv" CV "FALSE" Compa "TRUE" seuil "1" poolAsPool1L "TRUE" sampleMetadata_out "$scriptdir/output-sampleMetadata.tsv" variableMetadata_out "$scriptdir/output-variableMetadata.tsv" injectionOrderColName injectionOrder batchColName batch sampleTypeColName MySampType sampleTypeTags blank=blanc,pool=lot,sample=echant || exit 1
 
 diff $scriptdir/output-sampleMetadata.tsv $scriptdir/test-data/output-sampleMetadata-customSampleType.tsv || exit 2
 diff $scriptdir/output-variableMetadata.tsv $scriptdir/test-data/output-variableMetadata.tsv || exit 2
